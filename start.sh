d1f39c381025b9e17c60a11d44ffa89ed5d866f1
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2022-02-15 17:28:16 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-echo -e "1\n\n${STATUS_URL}\n\n&{USERNAME}\n${PASSWORD}\n\n" | bash /opt/status.sh c
-bash /opt/entrypoint.sh
+echo -e "1\n\n${STATUS_URL}\n\n&{USERNAME}\n${PASSWORD}\n\n" | sh -c /opt/status.sh c
+sh -c /opt/entrypoint.sh
