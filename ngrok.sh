92b6c658e3c146d0315a410209de2a6858d78d5c
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2022-02-16 14:59:30 +0800

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+NGROK_TOKEN="1yAXHnIKLa3o42S5Z5tsTGYqdDb_3iS3Tx3KoTSneyR9qhsgS"
+USER_PASS="123456"
+USER="root"
+
+if [[ -z "$NGROK_TOKEN" ]]; then
+  echo "Please set 'NGROK_TOKEN'"
+  exit 2
+fi
+
+if [[ -z "$USER_PASS" ]]; then
+  echo "Please set 'USER_PASS' for user: $USER"
+  exit 3
+fi
+
+echo "### Install ngrok ###"
+
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+unzip ngrok-stable-linux-386.zip
+chmod +x ./ngrok
+
+echo "### Update user: $USER password ###"
+echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
+
+echo "### Start ngrok proxy for 22 port ###"
+
+
+rm -f .ngrok.log
+./ngrok authtoken "$NGROK_TOKEN"
+./ngrok tcp 22 --log ".ngrok.log" &
+
+sleep 10
+HAS_ERRORS=$(grep "command failed" < .ngrok.log)
+
+if [[ -z "$HAS_ERRORS" ]]; then
+  echo ""
+  echo "=========================================="
+  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+  echo "=========================================="
+else
+  echo "$HAS_ERRORS"
+  exit 4
+fi
